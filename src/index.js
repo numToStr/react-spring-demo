f7845d003e3fa7cabcff3b17ce12b8feac8fc172
Vikas Raj <vikasraj1911@gmail.com>
2019-05-31 17:53:36 +0530

    added now.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 // import UseTransition from "./UseTransition";
 // import UseSpring from "./UseSpring";
 // import UseTrail from "./UseTrail";
-// import Drawer from "./Drawer";
+import Drawer from "./Drawer";
 // import MouseTrail from "./MouseTrail";
 import MouseTrailCanvas from "./MouseTrailCanvas";
 
@@ -16,7 +16,7 @@ function App() {
       {/* <UseTransition /> */}
       {/* <UseSpring /> */}
       {/* <UseTrail /> */}
-      {/* <Drawer /> */}
+      <Drawer />
       {/* <MouseTrail /> */}
       <MouseTrailCanvas />
     </div>
